ec8b22fd0def490d9083883867ae887db8cec4da
Luke Cahill <lmc@outlook.com>
2015-12-12 14:55:25 +0000

    Added comments. Check that the server contains nothing but numbers. Shouldn't be able to do this but double checking that they haven't

--- Chat/Chat.cs
+++ Chat/Chat.cs
@@ -15,11 +15,19 @@ namespace Chat {
 		private Socket server;
 		private Socket remoteClient;
 
+		/// <summary>
+		/// Initialise the form.
+		/// </summary>
 		public chatForm() {
 			InitializeComponent();
 			CheckForIllegalCrossThreadCalls = false;
 		}
 
+		/// <summary>
+		/// Click event which starts the inital connection
+		/// </summary>
+		/// <param name="sender">Button Object</param>
+		/// <param name="e">EventArgs arguments of the event</param>
 		private void connect_Click(object sender, EventArgs e) {
 			SetButtons(true, connect, disconnect, sendMessage);
 			try {
@@ -50,8 +58,17 @@ namespace Chat {
 			}
 		}
 
+		/// <summary>
+		/// Starts the server to accept the incoming client for messaging.
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		private void startServer_Click(object sender, EventArgs e) {
 			server = CreateSocketStream();
+			if(Regex.IsMatch(localPort.Text, @"[^0-9]")) {
+				MessageBox.Show("Local port must be numbers only!");
+				return;
+			}
 			var port = int.Parse(localPort.Text);
 
 			var localEndPoint = new IPEndPoint(0, port);
@@ -64,6 +81,11 @@ namespace Chat {
 			status.Text = $"Listenting for connections on port {port}";
 		}
 
+		/// <summary>
+		/// Handler for the send message click
+		/// </summary>
+		/// <param name="sender">Button object</param>
+		/// <param name="e">EventArgs of the button</param>
 		private void sendMessage_Click(object sender, EventArgs e) {
 			// Send the message here
 			var message = Encoding.ASCII.GetBytes(Message.Text);
@@ -71,49 +93,84 @@ namespace Chat {
 
 		}
 
+		/// <summary>
+		/// Button click handler for the disconnect button
+		/// </summary>
+		/// <param name="sender">Button Object</param>
+		/// <param name="e">EventArgs of the button</param>
 		private void disconnect_Click(object sender, EventArgs e) {
 			CloseConnection();
 			SetButtons(true, connect, disconnect, sendMessage);
 			status.Text = "Disconnected";
 		}
 
+		/// <summary>
+		/// Only allow for numbers, full stop, colon, and backspaces to be entered into the remote IP.
+		/// </summary>
+		/// <param name="sender">Textarea object</param>
+		/// <param name="e">KeyPressEventArgs arguments</param>
 		private void remoteIp_KeyPress(object sender, KeyPressEventArgs e) {
 			if (Regex.IsMatch(e.KeyChar.ToString(), @"[^0-9.:\b]")) {
 				e.Handled = true;
 			}
 		}
 
+		/// <summary>
+		/// Used to stop anything other than numbers being entered into the local port.
+		/// </summary>
+		/// <param name="sender">Textbox Object</param>
+		/// <param name="e">KeyPressEventArgs of the last key press</param>
 		private void localPort_KeyPress(object sender, KeyPressEventArgs e) {
 			if(Regex.IsMatch(e.KeyChar.ToString(), @"[^0-9\b]")) {
 				e.Handled = true;
 			}
 		}
 
+		/// <summary>
+		/// Displays a message box of how to use the program.
+		/// </summary>
 		private void helpToolStripMenuItem_Click(object sender, EventArgs e) {
 			MessageBox.Show("To use enter the IP, and port under the connect to header. This should be in the format of IPADDRESS:PORT; e.g. 127.0.0.1:1024.\n\nUnder the bind to port header enter the local port that the remote client should connect to, and the chat will then bind to that port and listen for incoming connections.", "Help");
 		}
 
+		/// <summary>
+		/// Start a new instance of the chat.
+		/// </summary>
 		private void newToolStripMenuItem_Click(object sender, EventArgs e) {
 			var info = new ProcessStartInfo(Application.ExecutablePath);
 			Process.Start(info);
 		}
 
+		/// <summary>
+		/// Exit the currect program
+		/// </summary>
 		private void exitToolStripMenuItem_Click(object sender, EventArgs e) {
 			Environment.Exit(0);
 		}
 
+		/// <summary>
+		/// Closes the socket.
+		/// Resets the form, and displays that it is disconnected.
+		/// </summary>
 		private void CloseConnection() {
 			if (client.Connected) {
 				client.Close();
 			}
 		}
 
+		/// <summary>
+		/// What happens after the client connects.
+		/// </summary>
+		/// <param name="result">IAsyncResult</param>
 		private void OnClientConnected(IAsyncResult result) {
 			client = server.EndAccept(result);
 			status.Text = $"Connected to: {remoteIp.Text}";
 			client.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(OnDataReceived), null);
 		}
 
+		/// <summary>
+		/// What to do when the message is sent
+		/// </summary>
 		private void OnDataSent(IAsyncResult result) {
 			try {
 				// handle the success and display the message.
@@ -127,6 +184,10 @@ namespace Chat {
 			}
 		}
 
+		/// <summary>
+		/// Handle the initial connection, sets the statusm and allows reciving to occur.
+		/// </summary>
+		/// <param name="result">IAsyncResult</param>
 		private void OnConnected(IAsyncResult result) {
 			try {
 				client.EndConnect(result);
@@ -137,6 +198,10 @@ namespace Chat {
 			}
 		}
 
+		/// <summary>
+		/// Handle when data is recieved.
+		/// </summary>
+		/// <param name="result">IAsyncResult</param>
 		private void OnDataReceived(IAsyncResult result) {
 			try {
 				// handle the recieved data
